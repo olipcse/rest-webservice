ebd998ae1fe430a51060853c2e9697b71ee09a0b
olipLinkstaff <ontohinolip@gmail.com>
2021-09-07 12:44:42 +0900

    converted user class to jpa entity

--- src/main/java/com/olip/rest/webservices/user/User.java
+++ src/main/java/com/olip/rest/webservices/user/User.java
@@ -3,22 +3,29 @@ package com.olip.rest.webservices.user;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 @Getter
 @Setter
+@Entity
 public class User {
-    public User(Integer id, String name, Date birthDate) {
-        this.id = id;
-        this.name = name;
-        this.birthDate = birthDate;
-    }
-
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min = 2,message = "Name should have at least 2 characters")
     private String name;
     @Past
     private Date birthDate;
+//    public User(Integer id, String name, Date birthDate) {
+//        this.id = id;
+//        this.name = name;
+//        this.birthDate = birthDate;
+//    }
+
+
 }
